bfc2c8899047253b178689252406ec8360147455
Eliza Wong <eliza@Eliza-MBP.local>
2021-07-13 16:27:08 -0400

    tests for form.test.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -55,7 +55,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
